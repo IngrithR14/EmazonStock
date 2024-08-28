de70d9f63caff1576188ee971e799c59321f493f
IngrithR14 <nenita.yr34@gmail.com>
2024-08-27 22:46:37 -0500

    Inicio

---
+++ src/test/java/com/example/EmazonStock/EmazonStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmazonStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
